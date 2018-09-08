e8aa7e561ec46db4fa11c094558074351e798c98
Brett Gonterman <brettgonterman@sbcglobal.net>
2018-09-08 15:54:02 -0500

    Update

--- assets/javascripts/Portfolio.js
+++ assets/javascripts/Portfolio.js
@@ -47,7 +47,7 @@ $(document).ready(function () {
     getData("ArrowGraphic")
     function getData(name) {
         $.ajax({
-            url: "./assets/images/" + name + ".html",
+            url: "Updated-Portfolio/assets/images/" + name + ".html",
             success: function (response) {
                 if (response == "false") {
                     alert("YA DUMMI");
