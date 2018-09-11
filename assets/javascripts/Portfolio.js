e012a68fe445d6d4150f09a86506fcc2614a7493
Brett Gonterman <brettgonterman@sbcglobal.net>
2018-09-11 18:06:37 -0500

    Update

--- assets/javascripts/Portfolio.js
+++ assets/javascripts/Portfolio.js
@@ -76,6 +76,7 @@ $(document).ready(function () {
     });
     /////////////////////
 
+
     function buildContact() {
         $("body").html(`<div class="wrapper hdrGrid">
         <div class="whtBkgrnd">
@@ -206,6 +207,13 @@ $(document).ready(function () {
 </div>
 <div class="wrapper footer"></div>
 <div class="bar"></div>`);
+        $(".third").hover(function () {
+            console.log("hmmm");
+            $(".aboutMeBox").css('margin-top', "90px");
+        }, function () {
+            console.log("yep");
+            $(".aboutMeBox").css('margin-top', "-260px");
+        });
     }
     function moreAboutMe() {
         $(".thirdB").html('<p class="text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi. <br> Tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem". Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>');
